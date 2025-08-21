95bb069318294232b9020ffb4789d3695b642c3b
Minkook <minguk2425@gmail.com>
2025-08-21 17:07:38 +0900

    first commit

--- src/component/Review.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/Review.css';
-export default function Review(){
-    return (
-        <>
-            <h4>리뷰</h4>
-            <input type="button" value="리뷰 작성"/>
-        </>
-    )
-}
--- src/component/UserInfo.jsx
+++
@@ -1,8 +0,0 @@
-export default function UserInfo(){
-  return (
-    <>
-      <img src="https://picsum.photos/50/50" alt=""/>
-      <h2>고객님의 정보</h2>
-    </>
-  );
-}
