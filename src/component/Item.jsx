e1fbd9b7eda55139f99e9fd7748b364a7b0f12f7
Minkook <minguk2425@gmail.com>
2025-08-21 18:04:05 +0900

    Item Detail Page Upgrade

--- src/component/Item.jsx
+++ src/component/Item.jsx
@@ -5,28 +5,16 @@ import { useParams } from "react-router-dom";
 import { getItems } from "../data";
 import '../styles/Item.css';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Item(){
+    const [mode,setMode]=useState('explain');
     const {id}=useParams();
     const itemList= getItems();
     const item=itemList.find((it)=>it.id === parseInt(id));
-    console.log(id);
-    return (
-        <>
-            <div id="item">
-                <Header></Header>
-                <img src="https://picsum.photos/150/150" alt="" />
-                <h4>{item.title}</h4>
-                <p>{item.price}</p>
-                <div>
-                    <input type="button" value="구매하기" />
-                    <input type="button" value="장바구니" />
-                </div>
-            </div>
-            <div>
-                <div>
-                    <h2>상품설명</h2>
-                    <p>
+    let content=null;
+    if(mode === 'explain'){
+        content=<p>
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                         Nemo vitae ipsum autem quas itaque nihil, tempora optio 
                         eius illum maiores doloremque assumenda consequuntur 
@@ -40,18 +28,57 @@ export default function Item(){
                         obcaecati saepe. Reprehenderit quasi rem quas itaque praesentium 
                         cumque illum blanditiis officia. Est repellendus, aspernatur corporis 
                         impedit voluptatum corrupti, earum ex omnis facilis doloremque similique sed.
-                    </p>
-                </div>
+        </p>;
+    } else if(mode === 'inquire'){
+        content=
+        <div>
+            <input type="text" />
+            <button>문의하기</button>
+        </div>;
+    } else if(mode === 'review'){
+        content=<Review></Review>;
+    }
+    return (
+        <>
+            <div id="item">
+                <Header></Header>
+                <img src="https://picsum.photos/150/150" alt="" />
+                <h4>{item.title}</h4>
+                <p>{item.price}</p>
                 <div>
-                    <h2>상품문의</h2>
-                    <button>문의하기</button>
+                    <input type="button" value="구매하기" />
+                    <input type="button" value="장바구니" />
                 </div>
+            </div>
+            <div className="nav">
+                <button onClick={()=>{
+                    setMode('explain');
+                }}>상품설명</button>
+                <button onClick={()=>{
+                    setMode('inquire');
+                }}>상품문의</button>
+                <button onClick={()=>{
+                    setMode('review');
+                }}>리뷰</button>
+            </div>
+            <div>
+                {content}
                 <div>
-                    <h2>배송 / 교환 / 환불 안내</h2>
-                    <h4>배송정보</h4>
-                    <p>롯데택배 / 무료배송</p>
-                    <h4>교환 / 반품</h4>
-                    <p>비용 : 6000원</p>
+                    <h2>배송 / 환불 / 교환 안내</h2>
+                    <table border={1}>
+                        <tr>
+                            <th>배송</th>
+                            <td>민국택배, 영업일 기준 2~3일</td>
+                        </tr>
+                        <tr>
+                            <th>환불</th>
+                            <td>수수료 10000원</td>
+                        </tr>
+                        <tr>
+                            <th>교환</th>
+                            <td>수수료 5000원</td>
+                        </tr>
+                    </table>
                 </div>
                 <div>
                     <h2>다른 상품들</h2>
@@ -66,7 +93,7 @@ export default function Item(){
                     </div>  
                 </div>
             </div>
-            <Review></Review>
+            
         </>
     )
 }
